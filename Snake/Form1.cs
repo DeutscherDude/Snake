431e2182d9eab1613c82a4d22ba810ac894694e9
Dawid Bańczak <dawid.banczak99@gmail.com>
2021-07-17 19:39:37 +0200

    Created first run methods

--- Snake/Form1.cs
+++ Snake/Form1.cs
@@ -12,10 +12,17 @@ namespace Snake
 {
     public partial class Form1 : Form
     {
+        Graphics graphics;
+        List<Tile> tiles;
+
         public Form1()
         {
             InitializeComponent();
+            graphics = gamePanel.CreateGraphics();
+            generateTiles();
         }
+        int _width = 16;
+        int _height = 16;
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -24,7 +31,18 @@ namespace Snake
 
         private void gamePanel_Paint(object sender, PaintEventArgs e)
         {
-
+            // namaluj se
+        }
+        private void generateTiles()
+        {
+            
+            for(int i = 0; i<_height; i++)
+            {
+                for(int j=0; j<_width; j++)
+                {
+                    // chuj TODO create tiles
+                }
+            }
         }
     }
 }
